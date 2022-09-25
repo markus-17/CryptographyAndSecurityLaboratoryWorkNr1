7525cf91d9330058781b9dc662f0cec22041bf19
markus-17 <marius.purici@isa.utm.md>
2022-09-26 00:38:43 +0300

    Implement AffineCipher

---
+++ src/main/scala/com/mariuspurici/cryptography_and_security/implementations/AffineCipher.scala
@@ -0,0 +1,22 @@
+package com.mariuspurici.cryptography_and_security.implementations
+
+import java.lang.Math.floorMod
+
+class AffineCipher(a: Int, b: Int) extends SubstitutionCipher {
+  private val alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  private val substitutedAlphabet: String = alphabet.map { char =>
+    (floorMod(
+      a * (char.toInt - 'A'.toInt) + b,
+      alphabetSize
+    ) + 'A'.toInt).toChar
+  }
+  private val alphabetToSubstituted: Map[Char, Char] =
+    (alphabet zip substitutedAlphabet).toMap
+  private val substitutedToAlphabet: Map[Char, Char] =
+    (substitutedAlphabet zip alphabet).toMap
+
+  override protected def encryptLetter(char: Char): Char =
+    alphabetToSubstituted(char)
+  override protected def decryptLetter(char: Char): Char =
+    substitutedToAlphabet(char)
+}
