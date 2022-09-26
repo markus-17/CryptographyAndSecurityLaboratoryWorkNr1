bb5e4720e2a4ee456136656accac5a76d58dada0
markus-17 <marius.purici@isa.utm.md>
2022-09-26 12:59:58 +0300

    Implement VigenereCipher

---
+++ src/main/scala/com/mariuspurici/cryptography_and_security/implementations/VigenereCipher.scala
@@ -0,0 +1,38 @@
+package com.mariuspurici
+package cryptography_and_security
+package implementations
+
+import java.lang.Math.floorMod
+
+class VigenereCipher(key: String) extends Cipher {
+  val transformedKey: String =
+    key.toUpperCase
+      .filter(char => 'A' <= char && char <= 'Z')
+      .distinct
+
+  val transformedKeyLength: Int = transformedKey.length
+
+  override def encrypt(message: String): String =
+    message.toUpperCase.zipWithIndex.map {
+      case (char, i) if 'A' <= char && char <= 'Z' =>
+        (floorMod(
+          (char.toInt - 'A'.toInt) + (transformedKey(
+            i % transformedKeyLength
+          ).toInt - 'A'.toInt),
+          alphabetSize
+        ) + 'A'.toInt).toChar
+      case (char, i) => char
+    }.mkString
+
+  override def decrypt(message: String): String =
+    message.toUpperCase.zipWithIndex.map {
+      case (char, i) if 'A' <= char && char <= 'Z' =>
+        (floorMod(
+          (char.toInt - 'A'.toInt) - (transformedKey(
+            i % transformedKeyLength
+          ).toInt - 'A'.toInt),
+          alphabetSize
+        ) + 'A'.toInt).toChar
+      case (char, i) => char
+    }.mkString
+}
